1f86f55bd6df2b2f0d7c0fdfa3ab28e0567bb2ef
Peafly78 <90190882+Peafly78@users.noreply.github.com>
2023-03-21 14:54:02 +0100

    enter boardgame data

--- bg_recommender.py
+++ bg_recommender.py
@@ -41,10 +41,15 @@ def display_numbered_options(numbered_options):
 
 # Data
 
-boardgame_types = ["family", "dexterity", "party", "abstract", "strategic", "thematic", "euro", "war", "dice", "cards", "deck building", "bluffing", "cooperative", "children's", "fighting", "area control", "legacy", "drafting", "memory", "miniatures", "engine building", "educational", "roleplaying"]
+boardgame_types = ["family", "dexterity", "party", "abstract", "strategic", "thematic", "euro", "war", "dice", "cards", "deck building", "bluffing", "cooperative", "children", "fighting", "area control", "legacy", "drafting", "memory", "miniatures", "engine building", "educational", "roleplaying"]
 
 boardgame_collection = {
     "dict_explanation" : [["tags/categories/types"], "minimum number of players", "maximum number of players", "average playing time in minutes", "minimum player age"]
-    "Braendi Dog" : [["family", "cooperative"], 3, 6, 60, 8]
+    "Braendi Dog" : [["family", "cooperative"], 3, 6, 60, 8],
+    "Ligretto" : [["cards", "abstract", "dexterity"], 2, 4, 20, 8],
+    "Ciao Giuseppe" : [["cards", "abstract"], 2, 6, 20, 6],
+    "Monopoly" : [["engine building", "strategic"], 2, 6, 120, 10],
+    "Spinderella" : [["family", "thematic"], 2, 4, 20, 6],
+    "Ligretto Dice" : [["dice", "abstract", "dexterity"], 2, 6, 10, 8]
 }
 
