c98a01348d8b3bd7727e826ca1cbf46a7199a793
Peafly78 <90190882+Peafly78@users.noreply.github.com>
2023-04-03 14:13:02 +0200

    add more games

--- games_data.py
+++ games_data.py
@@ -4,8 +4,16 @@ boardgame_collection = [
     ["Braendi Dog", ["family", "cooperative"], 3, 6, 45, 8],
     ["Ligretto", ["cards", "abstract", "dexterity"], 2, 4, 15, 8],
     ["Ciao Giuseppe", ["cards", "abstract"], 2, 6, 25, 6],
-    ["Monopoly", ["engine building", "strategic"], 2, 6, 30, 10],
+    ["Monopoly", ["engine building", "strategic"], 2, 6, 120, 10],
     ["Spinderella", ["family", "thematic"], 2, 4, 20, 6],
-    ["Ligretto Dice", ["dice", "abstract", "dexterity"], 2, 6, 10, 8]
+    ["Ligretto Dice", ["dice", "abstract", "dexterity"], 2, 6, 10, 8],
+    ["Bohnanza", ["cards", "party", "family"], 2, 7, 30, 8],
+    ["Settlers of Catan", ["engine building", "euro", "strategic"], 3, 4, 90, 10],
+    ["Catan Cards", ["cards", "deck building", "strategic"], 2, 4, 30, 10],
+    ["Lotti Karotti", ["family"], 2, 4, 20, 4],
+    ["Uno", ["cards", "abstract", "party"], 2, 8, 20, 6],
+    ["SkipBo", ["cards", "abstract"], 2, 6, 30, 8],
+    ["Skull King", ["cards", "thematic", "abstract"], 2, 8, 30, 9],
+    ["Dr. Eureka", ["dexterity", "family", "party"], 2, 4, 20, 6]
 ]
 
